158e2f06443d4d0ae401d93c92713eeada3df120
Mostafa-Abo-Elhassan <mostafamahmoudaboelhassn234@gmail.com>
2024-09-24 18:27:24 +0200

    Adding configuration of DATA ACCESS

---
+++ E-commerce_App_API/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce_App_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
--- E-commerce_App_API/obj/Debug/net8.0/E-commerce_App_API.AssemblyInfo.cs
+++ E-commerce_App_API/obj/Debug/net8.0/E-commerce_App_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce_App_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ed0ea5e4c958cfd74aef8f633d89b8d025635b")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce_App_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce_App_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
